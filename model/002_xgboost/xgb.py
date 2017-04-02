ac156edaffac67c6dffc5f2f486191e12c28915d
Guo Li <guoli@Guos-MacBook-Pro.local>
2017-04-01 20:07:35 -0700

    working version xgb script

---
+++ model/002_xgboost/xgb.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from sklearn.metrics import log_loss
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import KFold
+
+def run_xgb(train, features, target):
+    #parameters
+    params = {}
+    params["objective"]         = "reg:logistic"#"binary:logistic"
+    params["eval_metric"]       = "logloss"
+    params["booster"]           = "gbtree"
+    params["eta"]               = 0.05
+    params["tree_method"]       = 'exact'
+    params["max_depth"]         = 5
+    params["subsample"]         = 0.8
+    params["colsample_bytree"]  = 0.8
+    params["silent"]            = 1
+    params["nthread"]           = 4
+    params["seed"]              = 0
+#    params["scale_pos_weight"]  = 0.8
+
+    X_train, X_valid = train_test_split(train, test_size=0.30, random_state=0)
+    del train
+    
+    y_train = X_train[target]
+    y_valid = X_valid[target]
+    dtrain = xgb.DMatrix(X_train[features], y_train)
+    dvalid = xgb.DMatrix(X_valid[features], y_valid)
+    del X_train, X_valid, y_train, y_valid
+    
+    watchlist = [(dtrain, 'train'), (dvalid, 'eval')]
+    gbm = xgb.train(params, dtrain, 20000, evals=watchlist,
+    		early_stopping_rounds=30, verbose_eval=100)
+
+    del dtrain, dvalid
+    
+    return gbm
+
+nrows=50000
+
+path = "../../numeric/"
+df = pd.read_csv(path+"train_numeric.csv",nrows=nrows)
+
+target = "is_duplicate"
+features = list(df.columns.values)
+features.remove(target)
+
+kf = KFold(len(df.index),5)
+scores = []
+for train_index, test_index in kf:
+    train, valid = df.iloc[train_index,:], df.iloc[test_index,:]
+    gbm = run_xgb(train,features,target)
+    Y = gbm.predict(xgb.DMatrix(valid[features]),ntree_limit=gbm.best_iteration)
+    score = log_loss(valid[target],Y)
+    scores.append(score)
+print np.mean(scores), np.std(scores)
