23b241c2059581afa7cc62f8999a3239cd29fe9c
Guo Li <guoli@lbl.gov>
2017-04-03 16:28:49 -0700

    not ascii characters

--- data/src/100_nonunicode.py
+++ data/src/100_nonunicode.py
@@ -1,18 +1,32 @@
 #!/home/guoli/anaconda2/bin/python
 import string
 import chardet
+import re
 from nltk.stem.snowball import SnowballStemmer
+from collections import Counter
 
 stemmer = SnowballStemmer("english")
 
-def wordstem(fname):
+def non_ascii(fname):
     f = open(fname,"r")
     lines = f.readlines()
     f.close()
-    
-    for line in lines[:50]:
+
+    nasc = []    
+    for line in lines:
         words = line[:-1].split(" ")
         for word in words:
-            if chardet.detect(word)["encoding"] != 'ascii':
-                print word, [ord(c) for c in word] #chardet.detect(word)["encoding"], word.decode(chardet.detect(word)["encoding"]).encode('ascii')
-wordstem("destop_depunct_clean_train.csv")
+            if chardet.detect(word)["encoding"] != 'ascii' and word!='':
+                char_list = []
+                for char in word:
+                    if chardet.detect(char)["encoding"]=='ascii':
+                        char_list.append(" ")
+                    else:
+                        char_list.append(char)
+                nasc += "".join(char_list).split()
+    return nasc
+
+nascs = non_ascii("depunct_clean_train.csv")
+nascs += non_ascii("depunct_clean_test.csv")
+for nasc in set(nascs):
+    print nasc+","
